7ac173f70491a79ace28b9f98505e6bfa393497b
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2026-01-27 20:08:04 +0100

    git commit -m "feat(auth): permission-aware dashboard UI"

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,25 +1,49 @@
 import { auth } from '@/auth';
+import {
+  hasPermission,
+  permissions,
+} from '@/modules/auth/permissions';
 
 export default async function Dashboard() {
   const session = await auth();
+  const role = session!.user.role;
 
   return (
     <div>
       <h1 className="text-2xl font-semibold">Dashboard</h1>
 
       <p className="text-sm text-muted-foreground">
-        Hallo <strong>{session?.user.email}</strong> ({session?.user.role})
+        Du bist angemeldet als <strong>{session?.user.email}</strong> (
+        {session?.user.role})
       </p>
 
       <ul className="flex flex-col  md:justify-between md:flex-row gap-4 m-6">
-        <li className="md:min-w-50 flex justify-center rounded border p-4">
-          Produkte
+        <li className="flex flex-col border p-4 rounded-lg items-start">
+          <h2 className="mb-4 font-medium">Produkte</h2>
+          {hasPermission(role, permissions.product.create) && (
+            <button className="text-sm underline">Produkt anlegen</button>
+          )}
+          {hasPermission(role, permissions.product.update) && (
+            <button className="text-sm underline">
+              Produkte bearbeiten
+            </button>
+          )}
         </li>
-        <li className="md:min-w-50 flex justify-center rounded border p-4">
-          Bestellungen
+        <li className="flex flex-col border p-4 rounded-lg items-start">
+          <h2 className="font-medium mb-4 ">Bestellungen</h2>
+          {hasPermission(role, permissions.order.read) && (
+            <button className="text-sm underline">
+              Bestellungen ansehen
+            </button>
+          )}
         </li>
-        <li className="md:min-w-50 flex justify-center rounded border p-4">
-          Benutzer
+        <li className="flex flex-col border p-4 rounded-lg items-start">
+          <h2 className="mb-4 font-medium">Benutzer</h2>
+          {hasPermission(role, permissions.user.manage) && (
+            <button className="text-sm underline">
+              Benutzer verwalten
+            </button>
+          )}
         </li>
       </ul>
     </div>

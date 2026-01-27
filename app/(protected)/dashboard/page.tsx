5c7c63578a75fb8b997427795661099487543ddd
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2026-01-27 19:30:55 +0100

    git commit -m "feat(dashboard): add initial dashboard landing page"

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,9 +1,27 @@
-interface DashboardProps {}
+import { auth } from '@/auth';
 
-export default function Dashboard({}: DashboardProps): React.JSX.Element {
+export default async function Dashboard() {
+  const session = await auth();
+
   return (
-    <>
-      <h1>Dashboard</h1>
-    </>
+    <div>
+      <h1 className="text-2xl font-semibold">Dashboard</h1>
+
+      <p className="text-sm text-muted-foreground">
+        Hallo <strong>{session?.user.email}</strong> ({session?.user.role})
+      </p>
+
+      <ul className="flex flex-col  md:justify-between md:flex-row gap-4 m-6">
+        <li className="md:min-w-50 flex justify-center rounded border p-4">
+          Produkte
+        </li>
+        <li className="md:min-w-50 flex justify-center rounded border p-4">
+          Bestellungen
+        </li>
+        <li className="md:min-w-50 flex justify-center rounded border p-4">
+          Benutzer
+        </li>
+      </ul>
+    </div>
   );
 }

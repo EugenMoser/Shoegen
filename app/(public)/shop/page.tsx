9d2bf6cb59aea33dcfb56adb4bbe6f6b68bf8341
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2026-03-18 21:53:45 +0100

    feat(shop): refactor ShopPage to use searchQuery parameter and update component imports

--- app/(public)/shop/page.tsx
+++ app/(public)/shop/page.tsx
@@ -1,11 +1,12 @@
 import { Suspense } from 'react';
 
-import Search from '@/components/Search';
+import ShoeFilter from '@/components/ShoeFilter';
+import ShoeSearch from '@/components/ShoeSearch';
 import { getShoes } from '@/modules/shoes/actions/getShoe';
 import ProductCard from '@/modules/shoes/components/ProductCard';
 
 interface ShopPageProps {
-  searchParams?: Promise<{ query?: string }>;
+  searchParams?: Promise<{ searchQuery?: string }>;
 }
 
 export default async function ShopPage({
@@ -13,9 +14,9 @@ export default async function ShopPage({
 }: ShopPageProps): Promise<React.JSX.Element> {
   // Extract query from search parameters
   const params = await searchParams;
-  const query = params?.query || undefined;
-  // Fetch active shoes with optional search query, if query is undefined, it will fetch all active shoes
-  const shoesResult = await getShoes({ isActive: true, query });
+  const searchQuery = params?.searchQuery || undefined;
+  // Fetch active shoes with optional search query, if searchQuery is undefined, it will fetch all active shoes
+  const shoesResult = await getShoes({ isActive: true, searchQuery });
 
   if (!shoesResult.success) {
     return <div>Error: {shoesResult.error}</div>;
@@ -25,9 +26,14 @@ export default async function ShopPage({
   return (
     <>
       <h1>Shop Page</h1>
-      <Suspense fallback={<div>Loading search...</div>}>
-        <Search />
-      </Suspense>
+      <div className=" flex mb-4">
+        <Suspense fallback={<div>Loading search...</div>}>
+          <ShoeSearch />
+        </Suspense>
+        <Suspense fallback={<div>Loading filters...</div>}>
+          <ShoeFilter shoes={activeShoes} />
+        </Suspense>
+      </div>
       <p>Hier findest du eine Auswahl unserer besten Schuhe!</p>
 
       {activeShoes.length > 0 && (
